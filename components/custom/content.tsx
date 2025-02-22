1dc62777f40d2f8796a0f9d7486939cdb6d59480
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-02-22 22:45:34 +0300

    Update content.tsx

--- components/custom/content.tsx
+++ components/custom/content.tsx
@@ -10,7 +10,7 @@ export default function Content({ children, breadcrumbs }: ContentLayoutProps) {
     return (
         <div className='h-full w-full flex flex-col'>
             <Header breadcrumbs={breadcrumbs} />
-            <div className="flex-1 w-full h-full overflow-auto p-4 ">
+            <div className="flex-1 w-full h-full overflow-auto p-2">
                 <div className='bg-sidebar w-full h-full rounded-lg p-2'>
                     {children}
                 </div>
