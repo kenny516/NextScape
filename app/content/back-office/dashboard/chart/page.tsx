b5ae8eec1a2eebfa128e090a0a7a1cefce85ebc9
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-02-24 18:44:30 +0300

    Update page.tsx

--- app/content/back-office/dashboard/chart/page.tsx
+++ app/content/back-office/dashboard/chart/page.tsx
@@ -1,32 +1,18 @@
 "use client"
 import ChartLine from '@/components/back-office/chart/chart-line';
 import Content from '@/components/custom/content';
-import { useUserStore } from '@/stores/useUserStore';
 import { BreadcrumbItem } from '@/types';
-import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 
 export default function Page() {
     const breadcrumbs: BreadcrumbItem[] = [
         { label: 'Dashboard', href: '/content/back-office' },
         { label: 'chart' },
     ];
-
-    const { data: session } = useSession();
-    const { user, setUser } = useUserStore();
-    console.log(session);
-    useEffect(() => {
-        if (session?.user && user === null) {
-            setUser(session.user);
-        }
-    }, [session, setUser, user]);
-
     return (
         <Content breadcrumbs={breadcrumbs}>
             <div className='size-1/2'>
                 <ChartLine />
             </div>
         </Content>
-
     )
 }
