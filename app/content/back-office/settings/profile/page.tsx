dbdab545dab8f10bf21755d6be770a4d4e59ca01
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-02-28 18:44:34 +0300

    Update page.tsx

--- app/content/back-office/settings/profile/page.tsx
+++ app/content/back-office/settings/profile/page.tsx
@@ -1,7 +1,6 @@
 import Content from '@/components/custom/content';
 import { ProfileDetail } from '@/components/custom/profile/profile-detail'
 import { BreadcrumbItem } from '@/types';
-import Head from 'next/head';
 import React from 'react'
 
 export default function Page() {
@@ -11,9 +10,6 @@ export default function Page() {
     ];
     return (
         <Content breadcrumbs={breadcrumbs}>
-            <Head>
-                <title>profile</title>
-            </Head>
             <ProfileDetail />
         </Content>
 
