519f386cc63a8e08bed960a3f06ff50b3d91ea54
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-16 12:29:56 +0300

    update call function of better auth

--- components/custom/profile/profile-detail.tsx
+++ components/custom/profile/profile-detail.tsx
@@ -1,12 +1,12 @@
-"use client"
 import { Badge } from '@/components/ui/badge';
-import { useSession } from 'next-auth/react';
+import { authClient } from '@/lib/auth-client';
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react'
 
-export function ProfileDetail() {
-    const { data: session } = useSession();
+export async function ProfileDetail() {
+    const session = await authClient.getSession()
+    const user = session?.data?.user;
     return (
         <div className='w-full h-full m-auto '>
             <Head>
@@ -15,10 +15,10 @@ export function ProfileDetail() {
             <div className='h-1/4 flex flex-col items-center border-4  border-e-red-50 '>
                 <h1>Profile</h1>
                 <div>
-                    <Image className='rounded-full' src={session?.user?.image ?? 'https://ferf1mheo22r9ira.public.blob.vercel-storage.com/avatar-02-albo9B0tWOSLXCVZh9rX9KFxXIVWMr.png'} alt='avatar' width={100} height={100} />
+                    <Image className='rounded-full' src={user?.image ?? 'https://ferf1mheo22r9ira.public.blob.vercel-storage.com/avatar-02-albo9B0tWOSLXCVZh9rX9KFxXIVWMr.png'} alt='avatar' width={100} height={100} />
                 </div>
-                <Badge variant={'default'}>{session?.user?.email}</Badge>
-                <p>{session?.user?.name}</p>
+                <Badge variant={'default'}>{user?.email}</Badge>
+                <p>{user?.name}</p>
             </div>
         </div>
     )
